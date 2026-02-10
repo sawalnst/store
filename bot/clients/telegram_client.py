f2c9035971269bf44dedab6105175e4d85ae9bec
sawalnst <sawalnasution925@gmail.com>
2026-02-10 19:31:33 +0700

    telegram_client.py

--- bot/clients/telegram_client.py
+++ bot/clients/telegram_client.py
@@ -1,3 +1,4 @@
-from telegram import Bot
+from aiogram import Bot
+from bot.config import BOT_TOKEN
 
-bot = Bot(token="8389803915:AAHiPIZDbLdt2JTYL7Z0Y_vFvF6IKASZ9Bw")
+bot = Bot(token= 8582890088:AAE6jA8_Hu_HvsAYMyoaQYA2DOY2c-XOsjM)
