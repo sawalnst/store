cf1186e8899257179e497d9a3c4d01c3c935347c
sawalnst <sawalnasution925@gmail.com>
2026-02-10 20:31:48 +0700

    Delete bot/clients directory

--- bot/clients/__init__.py
+++
@@ -1,7 +0,0 @@
-from .telegram_client import bot
-from .ton_client import check_payment
-
-__all__ = [
-    "bot",
-    "check_payment",
-]
--- bot/clients/telegram_client.py
+++
@@ -1,4 +0,0 @@
-from aiogram import Bot
-from bot.config import BOT_TOKEN
-
-bot = Bot(token= 8582890088:AAE6jA8_Hu_HvsAYMyoaQYA2DOY2c-XOsjM)
