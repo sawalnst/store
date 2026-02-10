51ddabe01fc6803f7c803da203f90737b22bdaad
sawalnst <sawalnasution925@gmail.com>
2026-02-10 19:25:57 +0700

    telegram_client.py

--- bot/clients/telegram_client.py
+++ bot/clients/telegram_client.py
@@ -4,5 +4,4 @@ from telegram import Bot
 
 load_dotenv()
 
-bot = Bot(token=os.getenv("8389803915 : AAHiPIZDbLdt2JTYL7Z0Y_vFvF6IKASZ9Bw
-"))
+bot = Bot(token=os.getenv("8389803915 : AAHiPIZDbLdt2JTYL7Z0Y_vFvF6IKASZ9Bw"))
